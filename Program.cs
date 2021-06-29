075c025345ce22a3fb132c3d311ace79ced09600
ThomasMoerschel <tj.moerschel@gmail.com>
2021-06-29 09:02:49 -0400

    Updated House and Program Files

--- Program.cs
+++ Program.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using HouseFunction;
+
 
 namespace HelloWord
 {
     class Program
     {
+        private static string _first = "Hello";
+        private static string _last = "World!";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Lets hope this works!");
-            Console.WriteLine("Push update example");
+            
+            Console.WriteLine("This can be whatever I want!");
+
+            Test obj = new Test();
+            obj.SomeMethod();
+
+            Console.WriteLine(_first + " " + _last);
+            Console.WriteLine(obj.SomeMethod());
+            
+
+            House objHouse = new House();
+            Console.WriteLine(objHouse.getColor());
+            objHouse.setColor("Blue");
+            Console.WriteLine(objHouse.getColor());
+            objHouse.Price = 800000;
+            Console.WriteLine("Thomas' house was bought for $" + objHouse.Price);
+            objHouse.Name = "Thomas' very Expensive House";
+            Console.WriteLine(objHouse.Name);
+        }
+    }
+    class Test
+    {
+        public string SomeMethod()
+        {
+            return "Hello World SomeMethod";
         }
     }
 }
